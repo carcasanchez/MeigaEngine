118b96b301173d4f439026cb549c7e9a37a45a4a
Cabreira <carcasanchez@gmail.com>
2021-04-28 16:51:10 +0200

    WIP

--- Box.cpp
+++ Box.cpp
@@ -1,13 +1,15 @@
 #include "Box.h"
 #include "BindableBase.h"
 #include "GraphicsThrowMacros.h"
+#include "Cube.h"
 
 Box::Box(Graphics& gfx,
 	std::mt19937& rng,
 	std::uniform_real_distribution<float>& adist,
 	std::uniform_real_distribution<float>& ddist,
 	std::uniform_real_distribution<float>& odist,
-	std::uniform_real_distribution<float>& rdist)
+	std::uniform_real_distribution<float>& rdist,
+	std::uniform_real_distribution<float>& bdist)
 	:
 	r(rdist(rng)),
 	droll(ddist(rng)),
@@ -32,23 +34,12 @@ Box::Box(Graphics& gfx,
 	{
 		struct
 		{
-			float x;
-			float y;
-			float z;
+			DirectX::XMFLOAT3 pos;
 		} pos;
 	};
-	const std::vector<Vertex> vertices =
-	{
-		{ -1.0f,-1.0f,-1.0f },
-		{ 1.0f,-1.0f,-1.0f },
-		{ -1.0f,1.0f,-1.0f },
-		{ 1.0f,1.0f,-1.0f },
-		{ -1.0f,-1.0f,1.0f },
-		{ 1.0f,-1.0f,1.0f },
-		{ -1.0f,1.0f,1.0f },
-		{ 1.0f,1.0f,1.0f },
-	};
-	AddStaticBind(std::make_unique<VertexBuffer>(gfx, vertices));
+	auto model = Cube::Make<Vertex>();
+
+	AddStaticBind(std::make_unique<VertexBuffer>(gfx, model.vertices));
 
 	auto pvs = std::make_unique<VertexShader>(gfx, L"VertexShader.cso");
 	auto pvsbc = pvs->GetBytecode();
@@ -56,16 +47,8 @@ Box::Box(Graphics& gfx,
 
 	AddStaticBind(std::make_unique<PixelShader>(gfx, L"PixelShader.cso"));
 
-	const std::vector<unsigned short> indices =
-	{
-		0,2,1, 2,3,1,
-		1,3,5, 3,7,5,
-		2,6,3, 3,6,7,
-		4,5,7, 4,7,6,
-		0,4,2, 2,4,6,
-		0,1,4, 1,5,4
-	};
-	AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, indices));
+
+	AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, model.indices));
 
 	struct ConstantBuffer2
 	{
@@ -101,6 +84,8 @@ Box::Box(Graphics& gfx,
 
 	//Transform is not a static bind
 	AddBind(std::make_unique<TransformCbuf>(gfx, *this));
+
+	DirectX::XMStoreFloat3x3(&mt, DirectX::XMMatrixScaling(1.0f, 1.0f, bdist(rng)));
 }
 
 void Box::Update(float dt) noexcept
@@ -115,7 +100,8 @@ void Box::Update(float dt) noexcept
 
 DirectX::XMMATRIX Box::GetTransformXM() const noexcept
 {
-	return DirectX::XMMatrixRotationRollPitchYaw(pitch, yaw, roll) *
+	return DirectX::XMLoadFloat3x3(&mt) *
+		DirectX::XMMatrixRotationRollPitchYaw(pitch, yaw, roll) *
 		DirectX::XMMatrixTranslation(r, 0.0f, 0.0f) *
 		DirectX::XMMatrixRotationRollPitchYaw(theta, phi, chi) *
 		DirectX::XMMatrixTranslation(0.0f, 0.0f, 20.0f);
